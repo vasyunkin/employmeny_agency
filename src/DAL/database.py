2b1e3e3f149d645afbabac711abbeef8526f64d4
vasyunkin <vasunkon1@gmail.com>
2026-05-22 22:50:20 +0700

    актуализировал database.py

--- src/DAL/database.py
+++ src/DAL/database.py
@@ -1,19 +1,38 @@
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    async_sessionmaker
+)
 
 from src.DAL.repositories.sql_alchemy_unit_of_work import SqlAlchemyUnitOfWork
 from src.main.config import PostgresConfig
 
 
-def get_new_session_maker(
-        posgres_config: PostgresConfig
-    ) -> async_sessionmaker[AsyncSession]:
-        engine = create_async_engine(posgres_config.build_dsn())
-        return async_sessionmaker(engine, autoflush=False, expire_on_commit=False)
+postgres_config = PostgresConfig()
+DATABASE_URL = postgres_config.build_dsn()
 
 
-async def get_uow(session_maker: async_sessionmaker[AsyncSession]) -> SqlAlchemyUnitOfWork:
-    async with session_maker() as session:
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=postgres_config.enable_logging,
+    future=True,
+)
+
+
+async_session_factory = async_sessionmaker(
+    bind=engine,
+    expire_on_commit=False,
+)
+
+
+async def get_uow():
+    async with async_session_factory() as session:
         uow = SqlAlchemyUnitOfWork(session)
-        async with uow:
+
+        try:
             yield uow
+            await uow.commit()
+        except Exception:
+            await uow.rollback()
+            raise
+        finally:
+            await session.close()
