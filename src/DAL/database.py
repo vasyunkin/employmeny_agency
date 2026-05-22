34104755a92a2fe02d168dac9c90ef171227a126
vasyunkin <vasunkon1@gmail.com>
2026-05-22 22:35:12 +0700

    немного переделал database.py

--- src/DAL/database.py
+++ src/DAL/database.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 from sqlalchemy.ext.asyncio import create_async_engine
 
+from src.DAL.repositories.sql_alchemy_unit_of_work import SqlAlchemyUnitOfWork
 from src.main.config import PostgresConfig
 
 
@@ -9,3 +10,10 @@ def get_new_session_maker(
     ) -> async_sessionmaker[AsyncSession]:
         engine = create_async_engine(posgres_config.build_dsn())
         return async_sessionmaker(engine, autoflush=False, expire_on_commit=False)
+
+
+async def get_uow(session_maker: async_sessionmaker[AsyncSession]) -> SqlAlchemyUnitOfWork:
+    async with session_maker() as session:
+        uow = SqlAlchemyUnitOfWork(session)
+        async with uow:
+            yield uow
